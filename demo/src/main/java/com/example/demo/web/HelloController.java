f2af53d5eca3df2bc68c40b98d6a0b1e740a16af
Administrator <1931255708@qq.com>
2018-08-24 22:59:31 +0800

    test

---
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hl")
+    public String hl(){
+        return "hl";
+    }
+}
